26d0c78b1a525fa699dc791be59b281f72e3511b
JanodDev <janodpethum@gmail.com>
2025-08-17 22:34:12 +0530

    add srtpkg and honeymoo pkg separtly

---
+++ src/components/honeymoonpackages.jsx
@@ -0,0 +1,58 @@
+import tour1 from "../assets/tour1.jpg";
+import tour2 from "../assets/tour2.jpg";
+import tour3 from "../assets/tour3.jpg";
+export const honeymoonpackages = [
+  {
+    id: 1,
+    title: "8-Days Sri Lanka Cultural & Nature Sightseeing Tour",
+    subtitle: "8-Days Tours",
+    duration: "8 Days",
+    image: tour1,
+    highlights: [
+      "Sigiriya Rock Fortress",
+      "Dambulla Cave Temple",
+      "Polonnaruwa Ancient City",
+      "Kandy Temple of the Tooth",
+      "Scenic Train to Ella",
+      "Yala Safari",
+      "Mirissa Coconut Hill",
+      "Galle Fort & Turtle Hatchery",
+    ],
+    rating: 4.8,
+    reviews: 124,
+  },
+  {
+    id: 2,
+    title: "Sri Lanka 3-Days South Coast Discovery Tour",
+    subtitle: "3 Days Tours",
+    duration: "3 Days",
+    image: tour2,
+    highlights: [
+      "Temple of the Tooth",
+      "Cultural Heritage Sites",
+      "Traditional Cuisine",
+    ],
+    rating: 4.9,
+    reviews: 89,
+  },
+  {
+    id: 3,
+    title: "10-Day Sri Lanka Heritage, Nature & Culture Tour",
+    subtitle: "10 Days Tours",
+    duration: "10 Days",
+    image: tour3,
+    highlights: [
+      "Climb Sigiriya Rock Fortress",
+      "Explore Ancient Polonnaruwa & Dambulla Caves",
+      "Wildlife Safari at Minneriya & Yala",
+      "Temple of the Tooth Relic in Kandy",
+      "Scenic train ride from Nuwara Eliya to Ella",
+      "Nine Arch Bridge & Little Adam’s Peak",
+      "Whale Watching in Mirissa",
+      "Galle Fort & Turtle Hatchery",
+      "Bentota Beach & Madu River Safari",
+    ],
+    rating: 4.9,
+    reviews: 156,
+  },
+];
